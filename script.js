525e2b1fd32426cad8aa16e3b6aad4775c15419c
Harouy <114831817+Harouy@users.noreply.github.com>
2022-12-16 11:52:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,11 @@
-let myNodelist = document.getElementsByTagName("LI");
-let k;
-for (k = 0; k < myNodelist.length; k++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("Delete");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[k].appendChild(span);
-}
-let close = document.getElementsByClassName("close");
-let i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-let list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+let myNodelist = document.getElementsByClassName("li");
 
-function newTask() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("Delete");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        let div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  }
+let list = document.querySelector('.header');
+list.addEventListener('click', (ev)=> {
+  if (ev.target.ClassName === 'li') {
+    console.log("nfar ")
+    ev.target.classList.toggle('checked');
+  }})
+
+
+
