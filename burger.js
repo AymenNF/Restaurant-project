a3a4fbad89289d2e6cf36dd899b7dfd5d56c6b99
AymenNF <95107803+AymenNF@users.noreply.github.com>
2022-12-17 14:30:03 +0100

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,1 +1,56 @@
+function myFunction() {
+    var x = document.getElementById("bread-top");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
+
+  function myFunction2() {
+    var x = document.getElementById("cheese");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("meat");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("salade");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction5() {
+    var x = document.getElementById("tomato");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction6() {
+    var x = document.getElementById("bread-bottom");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
